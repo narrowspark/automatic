02055c0b5509e548ceafd2f4bb53fd0c30051192
Daniel Bannert <daniel.bannert@trnd.com>
2018-04-18 13:37:06 +0200

    fixes #8

---
+++ src/Configurator/DependencyConfigurator.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+namespace Narrowspark\Discovery\Configurator;
+
+use Narrowspark\Discovery\Common\Configurator\AbstractConfigurator;
+use Narrowspark\Discovery\Common\Contract\Package as PackageContract;
+
+class DependencyConfigurator extends AbstractConfigurator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configure(PackageContract $package): void
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unconfigure(PackageContract $package): void
+    {
+
+    }
+}

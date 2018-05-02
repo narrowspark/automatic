e5ea312febe62faf7509043df80f5e169fef1e4d
Daniel Bannert <daniel.bannert@trnd.com>
2018-05-02 17:06:40 +0200

    added some polyfills

--- src/PolyfillManager.php
+++ src/PolyfillManager.php
@@ -4,6 +4,26 @@ namespace Narrowspark\Discovery;
 
 class PolyfillManager
 {
+    /**
+     * @var array
+     */
+    private static $extReplacer = [
+        'mb_strlen'   => 'symfony/polyfill-mbstring',
+        'iconv'       => 'symfony/polyfill-iconv',
+        'ctype_alnum' => 'symfony/polyfill-ctype',
+    ];
+
+    /**
+     * @var array
+     */
+    private static $phpReplacer = [
+        '5.6' => 'symfony/polyfill-php56',
+        '7.0' => 'symfony/polyfill-php70',
+        '7.1' => 'symfony/polyfill-php71',
+        '7.2' => 'symfony/polyfill-php72',
+        '7.3' => 'symfony/polyfill-php73',
+    ];
+
     public function __construct()
     {
 
@@ -11,6 +31,6 @@ class PolyfillManager
 
     public function replace(): void
     {
-
+        $replacer = [];
     }
 }

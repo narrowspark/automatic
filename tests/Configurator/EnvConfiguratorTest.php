586b75b1a028220e230403e2881e2739719d8fbc
Daniel Bannert <daniel.bannert@trnd.com>
2018-05-04 09:51:08 +0200

    fix test

--- tests/Configurator/EnvConfiguratorTest.php
+++ tests/Configurator/EnvConfiguratorTest.php
@@ -60,8 +60,8 @@ class EnvConfiguratorTest extends MockeryTestCase
     {
         parent::tearDown();
 
-        \unlink($this->envDistPath);
-        \unlink($this->envPath);
+        @\unlink($this->envDistPath);
+        @\unlink($this->envPath);
     }
 
     public function testGetName(): void

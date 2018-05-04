df4c989e3d0e075b1a4856ebcf661fb38a443a12
Daniel Bannert <d.bannert@anolilab.de>
2018-05-04 21:48:18 +0200

    moved file copy to split.sh

--- build/travis/split.sh
+++ build/travis/split.sh
@@ -5,6 +5,10 @@ source ./build/travis/tfold.sh
 
 git subsplit init git@github.com:narrowspark/discovery.git
 
+cp ./build/common/composer.json ./.subsplit/src/Common/composer.json
+cp ./build/common/README.md ./.subsplit/src/Common/README.md
+cp ./LICENSE ./.subsplit/src/Common/LICENSE
+
 component_array=(
     'src/Common:git@github.com:narrowspark/discovery-common.git'
 )

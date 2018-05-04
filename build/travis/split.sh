4c0c49f35625b2e601317ff7003ed46b92988806
Daniel Bannert <d.bannert@anolilab.de>
2018-05-04 22:01:07 +0200

    added files to the common folder

--- build/travis/split.sh
+++ build/travis/split.sh
@@ -5,10 +5,6 @@ source ./build/travis/tfold.sh
 
 git subsplit init git@github.com:narrowspark/discovery.git
 
-cp ./build/common/composer.json ./.subsplit/src/Common/composer.json
-cp ./build/common/README.md ./.subsplit/src/Common/README.md
-cp ./LICENSE ./.subsplit/src/Common/LICENSE
-
 component_array=(
     'src/Common:git@github.com:narrowspark/discovery-common.git'
 )

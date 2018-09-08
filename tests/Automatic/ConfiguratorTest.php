49819732d1381389438378583920661c9d16e213
Daniel Bannert <prisis@users.noreply.github.com>
2018-09-08 23:01:41 +0200

    added midddot to configurators messages (#66)
    | Q               | A
    | --------------- | ---
    | Bug fix?        | yes
    | New feature?    | no
    | BC breaks?      | no
    | Deprecations?   | no
    | Related tickets | -
    | License         | MIT
    | Doc PR          | -

--- tests/Automatic/ConfiguratorTest.php
+++ tests/Automatic/ConfiguratorTest.php
@@ -41,11 +41,11 @@ final class ConfiguratorTest extends AbstractConfiguratorTest
 
         $this->ioMock->shouldReceive('writeError')
             ->once()
-            ->with(['    Copying files'], true, IOInterface::VERBOSE);
+            ->with(['    · Copying files'], true, IOInterface::VERBOSE);
 
         $this->ioMock->shouldReceive('writeError')
             ->once()
-            ->with(['    Created <fg=green>"' . $this->copyFileName . '"</>'], true, IOInterface::VERBOSE);
+            ->with(['    · Created <fg=green>"' . $this->copyFileName . '"</>'], true, IOInterface::VERBOSE);
 
         $this->configurator->configure($package);
 
@@ -67,11 +67,11 @@ final class ConfiguratorTest extends AbstractConfiguratorTest
 
         $this->ioMock->shouldReceive('writeError')
             ->once()
-            ->with(['    Copying files'], true, IOInterface::VERBOSE);
+            ->with(['    · Copying files'], true, IOInterface::VERBOSE);
 
         $this->ioMock->shouldReceive('writeError')
             ->once()
-            ->with(['    Created <fg=green>"' . $this->copyFileName . '"</>'], true, IOInterface::VERBOSE);
+            ->with(['    · Created <fg=green>"' . $this->copyFileName . '"</>'], true, IOInterface::VERBOSE);
 
         $this->configurator->configure($package);
 
@@ -79,11 +79,11 @@ final class ConfiguratorTest extends AbstractConfiguratorTest
 
         $this->ioMock->shouldReceive('writeError')
             ->once()
-            ->with(['    Removing files'], true, IOInterface::VERBOSE);
+            ->with(['    · Removing files'], true, IOInterface::VERBOSE);
 
         $this->ioMock->shouldReceive('writeError')
             ->once()
-            ->with(['    Removed <fg=green>"' . $this->copyFileName . '"</>'], true, IOInterface::VERBOSE);
+            ->with(['    · Removed <fg=green>"' . $this->copyFileName . '"</>'], true, IOInterface::VERBOSE);
 
         $this->configurator->unconfigure($package);
 

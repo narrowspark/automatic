862c12c38cb9bee452da57abf5a027a41f4942fb
danielbannert <daniel.bannert@limango.de>
2018-09-19 09:40:45 +0200

    remove unused class

--- src/Security/Contract/Exception/InvalidArgumentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace Narrowspark\Automatic\Security\Contract\Exception;
-
-use InvalidArgumentException as BaseInvalidArgumentException;
-
-class InvalidArgumentException extends BaseInvalidArgumentException implements Exception
-{
-}

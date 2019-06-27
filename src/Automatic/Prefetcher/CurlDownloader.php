e9f6ac56710147673a8969bf53f523b2ec1e8b36
Daniel Bannert <prisis@users.noreply.github.com>
2019-06-27 12:11:20 +0200

    fix: remove deprecated CURLPIPE_HTTP1 support flag (#116)

--- src/Automatic/Prefetcher/CurlDownloader.php
+++ src/Automatic/Prefetcher/CurlDownloader.php
@@ -78,7 +78,7 @@ final class CurlDownloader
     {
         $this->multiHandle = $mh = \curl_multi_init();
 
-        \curl_multi_setopt($mh, \CURLMOPT_PIPELINING, /*CURLPIPE_HTTP1 | CURLPIPE_MULTIPLEX*/ 3);
+        \curl_multi_setopt($mh, \CURLMOPT_PIPELINING, /*CURLPIPE_MULTIPLEX*/ 2);
 
         if (\defined('CURLMOPT_MAX_HOST_CONNECTIONS')) {
             \curl_multi_setopt($mh, \CURLMOPT_MAX_HOST_CONNECTIONS, 10);

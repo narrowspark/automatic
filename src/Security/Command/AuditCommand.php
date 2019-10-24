fb269077cf5919b77d8178014d6851071052fdc8
prisis <d.bannert@anolilab.de>
2019-10-24 08:40:19 +0200

    feat : adding github actions

--- src/Security/Command/AuditCommand.php
+++ src/Security/Command/AuditCommand.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of Narrowspark Framework.
+ *
+ * (c) Daniel Bannert <d.bannert@anolilab.de>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Narrowspark\Automatic\Security\Command;
 
 use Composer\Command\BaseCommand;
@@ -42,8 +51,8 @@ final class AuditCommand extends BaseCommand
             ->setDescription('Checks security issues in your project dependencies')
             ->setHelp(
                 <<<'EOF'
-The <info>%command.name%</info> command looks for security issues in the project dependencies.
-EOF
+                    The <info>%command.name%</info> command looks for security issues in the project dependencies.
+                    EOF
             );
     }
 
